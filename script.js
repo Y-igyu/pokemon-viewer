3eb0fe4fede66a04d205eb41bb089d2de4957a14
Y-igyu <145227350+Y-igyu@users.noreply.github.com>
2024-07-09 18:49:19 -0500

    Changes bg color by pokemon type

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+fetch("https://pokeapi.co/api/v2/pokemon/meltan")
   .then((res) => res.json())
   .then((data) => {
     let nameDiv = document.querySelector(".pokemon-name");
@@ -22,6 +22,67 @@ fetch("https://pokeapi.co/api/v2/pokemon/charmander")
       div.innerText = data.stats[i].base_stat + " " + data.stats[i].stat.name;
       stats.appendChild(div);
       console.log(stats);
+    }
+
+    let bg = document.querySelector("body");
+    switch (pokeType) {
+      case "Normal":
+        bg.style.backgroundColor = "#A8A878";
+        break;
+      case "Fire":
+        bg.style.backgroundColor = "#F08030";
+        break;
+      case "Water":
+        bg.style.backgroundColor = "#6890F0";
+        break;
+      case "Electric":
+        bg.style.backgroundColor = "#F8D030";
+        break;
+      case "Grass":
+        bg.style.backgroundColor = "#78C850";
+        break;
+      case "Ice":
+        bg.style.backgroundColor = "#98D8D8";
+        break;
+      case "Fighting":
+        bg.style.backgroundColor = "#C03028";
+        break;
+      case "Poison":
+        bg.style.backgroundColor = "#A040A0";
+        break;
+      case "Ground":
+        bg.style.backgroundColor = "#E0C068";
+        break;
+      case "Flying":
+        bg.style.backgroundColor = "#A890F0";
+        break;
+      case "Psychic":
+        bg.style.backgroundColor = "#F85888";
+        break;
+      case "Bug":
+        bg.style.backgroundColor = "#A8B820";
+        break;
+      case "Rock":
+        bg.style.backgroundColor = "#B8A038";
+        break;
+      case "Ghost":
+        bg.style.backgroundColor = "#705898";
+        break;
+      case "Dragon":
+        bg.style.backgroundColor = "#7038F8";
+        break;
+      case "Dark":
+        bg.style.backgroundColor = "#705848";
+        break;
+      case "Steel":
+        bg.style.backgroundColor = "#B8B8D0";
+        break;
+      case "Fairy":
+        bg.style.backgroundColor = "#F0B6BC";
+        break;
+      default:
+        bg.style.backgroundColor = "white";
+        break;
     }
 
     console.log(data);
